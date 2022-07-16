dd6793aff700b15d8c625036bbb41a9e80c3e40e
Sophy Kislik <s@192.168.1.6>
2022-07-16 13:45:06 +0300

    2 functions

--- foiders_creator.py
+++ foiders_creator.py
@@ -1,8 +1,18 @@
 import os
 
-i = 0
-limit = 300
 
-while i < limit:
-    os.mkdir("tmp/" + str(i))
-    i = i + 1
+def n_fc(limit):
+    i = 0
+    while i < limit:
+        os.mkdir("tmp/" + str(i))
+        i = i + 1
+
+
+def folders_creator(dirs: str):
+    dir_lst = dirs.split(' ')
+    for f in dir_lst:
+        os.mkdir('tmp/' + f)
+
+
+n_fc(3)
+folders_creator("cat dog mouse")
